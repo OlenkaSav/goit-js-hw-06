43bdd5a984f50d00237fdb1c2748487094275ba0
OlenkaSav <94956727+OlenkaSav@users.noreply.github.com>
2022-04-25 23:40:26 +0300

    task 1

---
+++ js/task-01.js
@@ -0,0 +1,14 @@
+const categories = document.querySelector('#categories');
+console.log(`Number of categories: ${categories.children.length}`);
+// console.log(categories.firstElementChild);
+
+// const first = categories.firstElementChild;
+// console.log(first.querySelector("h2").textContent);
+// console.log(first.querySelector("ul").children.length);
+
+const list =[...categories.children] ;
+// console.log(list);
+for (const item of list) {
+     console.log(`Category: ${item.querySelector("h2").textContent}`);
+console.log(`Elements: ${item.querySelector("ul").children.length}`);
+}
